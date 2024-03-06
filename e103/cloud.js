3aae9625e10c1ae633b8e724bc25abb357a8c1b6
Oscar Jacobsen <Oscar.Jacobsen@norconsult.com>
2024-03-06 14:16:02 +0100

    Create metadata.js

---
+++ e103/cloud.js
@@ -0,0 +1,51 @@
+{
+    "version": "1.8",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 455643979,
+    "boundingBox": {
+        "lx": 103017.13787841797,
+        "ly": 1208067.0939941407,
+        "lz": -6864.4287109375,
+        "ux": 109952.42786026001,
+        "uy": 1215002.3839759827,
+        "uz": 70.86127090454102
+    },
+    "tightBoundingBox": {
+        "lx": 103017.138,
+        "ly": 1208067.094,
+        "lz": -6864.429,
+        "ux": 104761.878,
+        "uy": 1211930.013,
+        "uz": 70.86099999999988
+    },
+    "pointAttributes": [
+        {
+            "name": "POSITION_CARTESIAN",
+            "size": 12,
+            "elements": 3,
+            "elementSize": 4,
+            "type": "int32",
+            "description": ""
+        },
+        {
+            "name": "RGBA",
+            "size": 4,
+            "elements": 4,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "classification",
+            "size": 1,
+            "elements": 1,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        }
+    ],
+    "spacing": 60.06137466430664,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
