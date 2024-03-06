0d57822c299fde01d18519ba6b16743b073f3170
Oscar Jacobsen <Oscar.Jacobsen@norconsult.com>
2024-03-06 12:12:50 +0100

    Create metadata.js

---
+++ e103-e102/metadata.js
@@ -0,0 +1,51 @@
+{
+    "version": "1.8",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 93472072,
+    "boundingBox": {
+        "lx": 104607.60182189942,
+        "ly": 1211507.4307861329,
+        "lz": -11.91046142578125,
+        "ux": 105166.38558959961,
+        "uy": 1212066.214553833,
+        "uz": 546.8733062744141
+    },
+    "tightBoundingBox": {
+        "lx": 104607.602,
+        "ly": 1211507.431,
+        "lz": -11.91,
+        "ux": 105166.386,
+        "uy": 1211984.002,
+        "uz": 54.552
+    },
+    "pointAttributes": [
+        {
+            "name": "POSITION_CARTESIAN",
+            "size": 12,
+            "elements": 3,
+            "elementSize": 4,
+            "type": "int32",
+            "description": ""
+        },
+        {
+            "name": "RGBA",
+            "size": 4,
+            "elements": 4,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "classification",
+            "size": 1,
+            "elements": 1,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        }
+    ],
+    "spacing": 4.83920955657959,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
